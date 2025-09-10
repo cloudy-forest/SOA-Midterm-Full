4cb13f0a354e34848779b516f2b46dfcf712ddc5
tinnt <tinnt@airtrip-intl.com>
2025-09-10 14:39:09 +0700

    fix env

--- SOA-2025-MIDTERM/main.py
+++ SOA-2025-MIDTERM/main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
-from core.database import engine, Base
-import models
-from api.routes_users import router as users_router
+from fastapi.middleware.cors import CORSMiddleware
+
 from api.routes_auth import router as auth_router
 from api.routes_payments import router as payment_router
 from api.routes_tuitions import router as tuition_router
+from api.routes_users import router as users_router
+from core.database import engine, Base
+from core.logger import logger
 # import redis
 from core.redis import redis_client
-
-from core.logger import logger
 
 Base.metadata.create_all(bind=engine)
 
@@ -21,6 +21,14 @@ app.include_router(users_router)
 app.include_router(payment_router)
 app.include_router(tuition_router)
 
+# add cors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 
 @app.on_event("startup")
@@ -28,8 +36,8 @@ async def startup_event():
     redis_client.set("startup_test", "FastAPI + Redis works!")
     logger.info("🚀 Application startup complete")
 
+
 @app.on_event("shutdown")
 async def shutdown_event():
     await redis_client.close()
     logger.info("🛑 Application shutdown")
-
--- SOA-2025-MIDTERM/services/user_service.py
+++ SOA-2025-MIDTERM/services/user_service.py
@@ -1,18 +1,22 @@
+import logging
+
 from sqlalchemy.orm import Session
 from models.tuition import Tuition
 from models.user import User
 from core.security import create_access_token, create_refresh_token, verify_token
-
+from fastapi import HTTPException
+from core.logger import logger
 
 
 def user_login(db:Session, username:str, password:str):
 
     exist_user = db.query(User).filter(User.username == username).first()
     if not exist_user:
-        return False
+        raise HTTPException(status_code=400, detail="Wrong username or password")
     elif exist_user.password != password:
-        return False
+        raise HTTPException(status_code=400, detail="Wrong username or password")
     else:
+        logger.info(exist_user.role.value)
         data = {
             "id": exist_user.id,
             "email": exist_user.email,
