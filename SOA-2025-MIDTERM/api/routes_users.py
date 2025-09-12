e71383e2e9814919bcba43b94ada63e05d09647f
tinnt <tinnt@airtrip-intl.com>
2025-09-12 16:08:54 +0700

    fetch user data

--- SOA-2025-MIDTERM/api/routes_users.py
+++ SOA-2025-MIDTERM/api/routes_users.py
@@ -14,6 +14,11 @@ async def get_user(current_user : models.User = Depends(get_current_user)):
         "email": current_user.email,
         "phone": current_user.phone,
         "fullname": current_user.fullname,
+        "address": current_user.address,
+        "available_balance": current_user.available_balance,
+        "role": current_user.role.value,
+        "username": current_user.username,
+        "studentId": current_user.id
     }
 
 
